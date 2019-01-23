79dbba7c5c98df8faa70650df20e13124a183412
Nathan Quach <nathanquach8x@gmail.com>
2019-01-24 05:21:02 +1100

    Add comment to scripts

--- bin/create-meta.js
+++ bin/create-meta.js
@@ -11,4 +11,15 @@ const {
   type
 } = parseProcessArgs()
 
+/**
+ * Create metadata.json for manga and chapter
+ * TODO: add support for volume next
+ *
+ * Usage:
+ * node bin/create-meta.js <folder-path> -t <content type>
+ *
+ * Example:
+ * node bin/create-meta.js /mnt/d/h -t manga
+ * node bin/create-meta.js /mnt/c/home/h/[author a]\ book b -t chapter
+ */
 createMetadataFiles(folderPath, type, limit)
--- bin/scan-folder.js
+++ bin/scan-folder.js
@@ -13,4 +13,11 @@ const {
   limit
 } = parseProcessArgs()
 
+/**
+ * Scan folders and create database record
+ *
+ * Usage: API=<api base url> node bin/scan-folder.js <folder-path> -l <limit number of directories>
+ *
+ * Example: API=http://192.168.0.2:8181/api node bin/scan-folder.js /mnt/c/home/h
+ */
 scanFolder(folderPath, createContent, {}, limit)
